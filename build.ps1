7c84874776bea551bd8b2988ed2366c13a9be9d1
chr_ <chr@chrxw.com>
2025-02-20 14:45:53 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,46 +1,18 @@
 
 $variants = "linux-x64", "linux-arm64", "win-x64", "win-arm64"
 $projectName = "ASF-Bot.Telegram"
-$frameworks = "net8.0", "net9.0"
 
-$jobs = @();
-
-foreach($framework in $frameworks){
 foreach ($variant in $variants) {
-    $buildArgs = '-r', "$variant", '-p:PublishSingleFile=true', '-p:IncludeNativeLibrariesForSelfExtract=true'
-    $commonArgs = '-p:PublishTrimmed=true'
+    $buildArgs = '-p:PublishSingleFile=true', '--self-contained=false'
+    $buildFdeArgs = '-p:PublishSingleFile=true', '--self-contained=true'
 
-    if (($variant -notlike "win-*") -and ($winProjectNames -contains "$projectName")) {
-        Write-Output "skip $projectName $variant"
-        continue
-    }
-    else {
-        $commonArgs = '-p:PublishTrimmed=false'
-    }
+    $commonArgs = '-r', "$variant", '-f', "net9.0", '-p:PublishTrimmed=true', '-p:IncludeNativeLibrariesForSelfExtract=true' , '-p:ContinuousIntegrationBuild=true', '--no-restore', '--nologo'
 
     Write-Output "start build $projectName $variant"
 
     dotnet restore $projectName -p:ContinuousIntegrationBuild=true --nologo
-    dotnet publish $projectName -c "Release" -f "$framework" -o "./dist/$variant/$projectName-$framework" --self-contained=true -p:ContinuousIntegrationBuild=true --no-restore --nologo $commonArgs $buildArgs
+    dotnet publish $projectName -c "Release" -o "./dist/$projectName-$variant" $commonArgs $buildArgs
+    dotnet publish $projectName -c "Release" -o "./dist/[fde]$projectName$variant" $commonArgs $buildFdeArgs
 
-    # $jobs += Start-Job -ScriptBlock {
-    #     param($variant)
-    #     7z a -bd -slp -tzip -mm=Deflate -mx=5 -mfb=150 -mpass=10 "./dist/$variant-fde.zip" "./dist/$variant-fde/*"
-    # } -ArgumentList $variant
-
-    # $jobs += Start-Job -ScriptBlock {
-    #     param($variant)
-    #     7z a -bd -slp -tzip -mm=Deflate -mx=5 -mfb=150 -mpass=10 "./dist/$variant.zip" "./dist/$variant/*"
-    # } -ArgumentList $variant
-}
-}
-
-# 等待所有后台任务完成
-foreach ($job in $jobs) {
-    Wait-Job -Job $job
-}
-
-# 获取任务的输出
-foreach ($job in $jobs) {
-    Receive-Job -Job $job
+    Write-Output "build $projectName $variant complete"
 }
