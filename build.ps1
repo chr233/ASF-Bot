b67656685cb31a410032474f06af8ee398f54a62
chr_ <chr@chrxw.com>
2025-11-22 18:24:04 +0800

    misc

--- build.ps1
+++ build.ps1
@@ -1,18 +1,31 @@
 
-$variants = "linux-x64", "linux-arm64", "win-x64", "win-arm64"
+$runtimes = "linux-x64", "linux-arm64", "win-x64", "win-arm64"
+$framework = "net10.0"
+$config = "Release"
 $projectName = "ASF-Bot.Telegram"
+$enableTrim = $true
+$trimMode = "link"
+$zip = $false
 
-foreach ($variant in $variants) {
-    $buildArgs = '-p:PublishSingleFile=true', '--self-contained=false', '-p:UseAppHost=true'
-    $buildFdeArgs = '-p:PublishSingleFile=true', '--self-contained=true'
+foreach ($runtime in $runtimes) {
+    Write-Debug "Publishing for runtime: $runtime and framework: $framework"
+    $buildName = "$projectName-$runtime-$framework"
 
-    $commonArgs = '-r', "$variant", '-f', "net9.0", '-p:PublishTrimmed=true', '-p:IncludeNativeLibrariesForSelfExtract=true' , '-p:ContinuousIntegrationBuild=true', '--no-restore', '--nologo'
+    $outputDir = "./dist/$buildName"
 
-    Write-Output "start build $projectName $variant"
+    dotnet publish $projectName --output "$outputDir" --self-contained --runtime $runtime --framework $framework --configuration $config --no-restore --nologo -p:PublishTrimmed=$enableTrim -p:TrimMode=$trimMode -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:ContinuousIntegrationBuild=true  -p:UseAppHost=true
+    dotnet publish $projectName --output "$outputDir" --no-self-contained --runtime $runtime --framework $framework --configuration $config --no-restore --nologo -p:PublishTrimmed=$enableTrim -p:TrimMode=$trimMode -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true -p:ContinuousIntegrationBuild=true  -p:UseAppHost=true
 
-    dotnet restore $projectName -p:ContinuousIntegrationBuild=true --nologo
-    dotnet publish $projectName -c "Release" -o "./dist/$projectName-$variant" $commonArgs $buildArgs
-    dotnet publish $projectName -c "Release" -o "./dist/[fde]$projectName$variant" $commonArgs $buildFdeArgs
+    Remove-Item "$outputDir-fde/*.xml"
+    Remove-Item "$outputDir/*.xml"
 
-    Write-Output "build $projectName $variant complete"
+    if ($true -eq $zip) {
+        Write-Debug "Creating zip archive for $buildName"
+        7z a -bd -slp -tzip -mm=Deflate -mx=5 -mfb=150 -mpass=10 "./dist/$buildName-fde.zip" "./tmp/$buildName-fde/*"
+        7z a -bd -slp -tzip -mm=Deflate -mx=5 -mfb=150 -mpass=10 "./dist/$buildName.zip" "./tmp/$buildName/*"
+    }
+}
+
+if ($true -eq $zip) {
+    Remove-Item -Recurse -Force "./tmp"
 }
